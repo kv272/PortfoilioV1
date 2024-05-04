c903618b6ae87ad47377f8a1dfcdf31b74774fc8
kv272 <vermakunal177@gmail.com>
2024-05-04 12:30:57 +0530

    email rectification

--- src/constants.js
+++ src/constants.js
@@ -12,7 +12,7 @@ export const dialogueData = {
   <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/kv272/PortfoilioV1/blob/main/public/KUNAL-Resume.pdf" target="_blank">Check it out?</a>
-  Contact me at vermakunal177@gmail.com if you have any interesting job opportunities!`,
+  Contact me at <a href = "vermakunal177@gmail.com">vermakunal117@gmail</a> if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas`,
   library: `I also read novels and books. my favorite ones are Crime and Punishment and the Mindhunter`,
