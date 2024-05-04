77a3831b45967715f3060536dcb17828dbb92e47
kv272 <vermakunal177@gmail.com>
2024-05-04 09:44:29 +0530

    final commit

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,20 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `My workspace, This is where i spend my most of the time these days.
+    I've made a couple of projects. I also like Java and Python. I am a probelm solver too. here is my leetcode profile - <a href = "https://leetcode.com/u/kunnuk2uk/">leetcode</a>.
+    and Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
+  "cs-degree": `This will be my CS degree once i graduate. It is a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I am learning full stack development these days.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/kv272/PortfoilioV1/blob/main/public/KUNAL-Resume.pdf" target="_blank">Check it out?</a>
+  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas`,
+  library: `I also read novels and books. my favorite ones are Crime and Punishment and the Mindhunter`,
+  exit: `If you want to exit Kunal's portfolio, just close the tab.`,
+};
